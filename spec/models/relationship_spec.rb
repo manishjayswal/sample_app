244585f8db423a26861d11b13514f35c507a1127
Manish-Jayswal <jayswalmp@gmail.com>
2019-09-03 13:17:19 +0545

    changing its

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
   describe 'Association' do
-    it { should belong_to(:follower).class_name("User") }
-    it { should belong_to(:followed).class_name("User") }
+    it { is_expected.to belong_to(:follower).class_name("User") }
+    it { is_expected.to belong_to(:followed).class_name("User") }
   end
 
   describe 'valid attributes' do
-    it { should validate_presence_of(:follower_id) }
-    it { should validate_presence_of(:followed_id) }
+    it { is_expected.to validate_presence_of(:follower_id) }
+    it { is_expected.to validate_presence_of(:followed_id) }
   end
 
 end
